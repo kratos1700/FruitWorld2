9971627155e6d65f05bee1ce7b582b3cfdd5a40f
David <hunterstarsa@hotmail.com>
2020-06-19 22:01:51 +0200

    creacio de la classe fruita

---
+++ app/src/main/java/com/example/fruitworld2/models/Fruita.java
@@ -0,0 +1,82 @@
+package com.example.fruitworld2.models;
+
+
+public class Fruita {
+
+    private final int VALOR_MAX = 10;
+    private  final int VALOR_INI=0;
+
+    // atributs
+    private String nombre;
+    private String descripcion;
+    private int  imagBack;
+    private  int icono;
+    private  int cantidad;
+
+    // constructors
+
+    public Fruita() {
+    }
+
+    public Fruita(String nombre, String descripcion, int imagBack, int icono, int cantidad) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.imagBack = imagBack;
+        this.icono = icono;
+        this.cantidad = cantidad;
+    }
+
+    // setter i getters
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getImagBack() {
+        return imagBack;
+    }
+
+    public void setImagBack(int imagBack) {
+        this.imagBack = imagBack;
+    }
+
+    public int getIcono() {
+        return icono;
+    }
+
+    public void setIcono(int icono) {
+        this.icono = icono;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    // metode per afegir cuantitats
+    public void addCuantitat(int cantidad){
+        if (this.cantidad < VALOR_MAX){
+            this.cantidad+=cantidad;
+        }
+    }
+    // metode resset
+    public  void resetCuantitat(){
+        this.cantidad = VALOR_INI;
+    }
+}
