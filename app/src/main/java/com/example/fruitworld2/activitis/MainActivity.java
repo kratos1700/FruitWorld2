2b3e5aa7e8715f88efd82da609670f6820d86ee3
David <hunterstarsa@hotmail.com>
2020-06-19 17:08:52 +0200

    preparacio dels paquets

--- app/src/main/java/com/example/fruitworld2/activitis/MainActivity.java
+++ app/src/main/java/com/example/fruitworld2/activitis/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.fruitworld2;
+package com.example.fruitworld2.activitis;
 //exercici 3
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.fruitworld2.R;
 
 public class MainActivity extends AppCompatActivity {
 
